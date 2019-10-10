0ad51282083be62463e996a71da21b87ab51d9e4
linguanghui <154068439@qq.com>
2019-10-10 15:50:01 +0800

    Squashed commit of the following:
    commit 9fe86fb92bf7802429b2981d31cbff6ba942c460
    Author: linguanghui <154068439@qq.com>
    Date:   Thu Oct 10 15:43:59 2019 +0800
    
        dfdsafd
    
    commit 9b4b465bdadb35affaff61255ea39f2116fea808
    Merge: ab7e1cd 6a4ec01
    Author: linguanghui <154068439@qq.com>
    Date:   Thu Oct 10 15:41:29 2019 +0800
    
        Merge branch 'dev'
    
        # Conflicts:
        #	User.java
    
    commit ab7e1cd69ebff7bf4ea36863423a42daa47092cd
    Author: linguanghui <154068439@qq.com>
    Date:   Thu Oct 10 15:37:38 2019 +0800
    
        dfsa
    
    # Conflicts:
    #	User.java

--- User.java
+++ User.java
@@ -1,7 +1,12 @@
 public class User{
 public  void static main (String[] args){
+	private int id;
                 private String userName;
 	private int age;
+
 	private int userId;
+
+	private String email;
+
 	}
 }
