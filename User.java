b48436246c076c09802807dd5327347a35f4188f
linguanghui <154068439@qq.com>
2019-10-12 10:28:03 +0800

    删除了UserId

--- User.java
+++ User.java
@@ -2,9 +2,6 @@ public class User{
 public  void static main (String[] args){
 	private int id;
                 private String userName;
-
-	private int userId;
-
 	private String email;
 	private String adress;
 	private String phone;
